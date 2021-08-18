368044ba7a15c1ba00f6b99d3c4f6fd2307a1e8f
the-macson <ak9991927952@gmail.com>
2021-08-18 18:50:52 +0530

    add style part

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from "react";
+import Input from "@material-ui/core/Input";
+// import Button from "@material-ui/core/Button";
+// import AddIcon from "@material-ui/icons/Add";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import IconButton from "@material-ui/core/IconButton";
+import "./App.css";
 function App() {
+  const [inputList, setInputList] = useState("buy apple");
+  const itemEvent =()=>{
+
+  }
   return (
-    <div className="App">
-     <h1>Hello Mukesh</h1>
-    </div>
+    <>
+      <div className="main">
+        <div class="center">
+          <h1>To Do List</h1>
+          <Input type="text" placeholder="Add your Task" className="task" onChange={itemEvent} />
+          <span className="icon">
+            <IconButton aria-label="add" color="primary" className="icon">
+              <AddCircleIcon fontSize="large" className="add" />
+            </IconButton>
+          </span>
+          <ol>
+            <li>
+              {inputList}
+            </li>
+          </ol>
+        </div>
+      </div>
+    </>
   );
 }
 
