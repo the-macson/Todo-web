958786e54c045fa3351979a1702e570e6013b75a
the-macson <ak9991927952@gmail.com>
2021-08-19 20:17:43 +0530

    added some js code

--- src/App.js
+++ src/App.js
@@ -3,28 +3,57 @@ import Input from "@material-ui/core/Input";
 // import Button from "@material-ui/core/Button";
 // import AddIcon from "@material-ui/icons/Add";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
+import CancelIcon from "@material-ui/icons/Cancel";
 import IconButton from "@material-ui/core/IconButton";
 import "./App.css";
 function App() {
-  const [inputList, setInputList] = useState("buy apple");
-  const itemEvent =()=>{
-
+  const [inputList, setInputList] = useState("");
+  const [Items,setItems] = useState([]);
+  const itemEvent =(event)=>{
+        setInputList(event.target.value);
+  }
+  const listOfItem=()=>{
+        setItems((oldItems) =>{
+          return [...oldItems,inputList];
+        })
   }
   return (
     <>
       <div className="main">
         <div class="center">
           <h1>To Do List</h1>
-          <Input type="text" placeholder="Add your Task" className="task" onChange={itemEvent} />
+          <Input
+            type="text"
+            placeholder="Add your Task"
+            className="task"
+            onChange={itemEvent}
+          />
           <span className="icon">
-            <IconButton aria-label="add" color="primary" className="icon">
+            <IconButton
+              aria-label="add"
+              color="primary"
+              className="icon"
+              onClick={listOfItem}
+            >
               <AddCircleIcon fontSize="large" className="add" />
             </IconButton>
           </span>
           <ol>
-            <li>
-              {inputList}
-            </li>
+            {Items.map((itemval) => {
+              return (
+                <li>
+                  <IconButton
+                    aria-label="cancel"
+                    color="primary"
+                    className="icon1"
+                  >
+                    <CancelIcon fontSize="large" />
+                  </IconButton>
+                  {itemval}
+                </li>
+              );
+            })}
+           
           </ol>
         </div>
       </div>
